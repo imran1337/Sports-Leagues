bdf7794ced632bcfce098b7637dee5194d5c245e
Coding Bird <Codingbird@yahoo.com>
2021-03-11 21:57:31 +0600

    __redirects added

--- src/Component/Leagues/Leagues.jsx
+++ src/Component/Leagues/Leagues.jsx
@@ -31,7 +31,7 @@ const Leagues = () => {
     <>
       {(leagues.length && (
         <div className="d-flex flex-wrap justify-content-center container-fluid">
-          {leagues.slice(0,800).map((league) => {
+          {leagues.slice(0,700).map((league) => {
             const {
               idLeague,
               strLeague,
